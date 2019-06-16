ec66fdfa78bba7e8e4777ed70685dc30ef51c98d
Mary McCready <mkmccre2@asu.edu>
2019-06-15 20:02:50 -0700

    Initial project commit with docs
    Ammeding commit to update author.

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,68 @@
+/**
+    * Author: Mary McCready
+    * Class: CSE 360 2019 Summer C (41792)
+    * Assignment #2
+    * This program adds.
+    */
+
+package cse360assign2;
+
+/**
+ * AddingMachine is the base class for the AddingMachine program.
+ * 
+ * @author marymccready
+ *
+ */
+public class AddingMachine {
+
+	/** The total amount. */
+	private int total;
+	
+	/** 
+	 * Sole class constructor for AddingMachine.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * The method used to get the current total.
+	 * 
+	 * @return total as int.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * The method used to add the parameter value to the total.
+	 * 
+	 * @param value (required) to be added to total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * The method used to subtract the parameter value from the total.
+	 * 
+	 * @param value (required) to be subtracted from total.
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * The method used to convert the output to a string.
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * The method used to clear the total.
+	 */
+	public void clear() {
+	
+	}
+}
